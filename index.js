005fd75fb48a7d039a43c7b82f38e25623a0a171
Connor Dailey <connorldailey@gmail.com>
2024-09-29 15:52:38 -0400

    Finish render license code. Add render questions section code. Complete initial generate markdown code.

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 import inquirer from 'inquirer';
 import fs from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
+    // Project Title
     {
         type: 'input',
         name: 'title',
         message: 'Enter the title of your project.',
     },
+    // Description section content
     {
         type: 'input',
         name: 'description',
         message: 'Provide a short description of your project.',
     },
+    // Installation section content
     {
         type: 'input',
         name: 'installation',
         message: 'Describe how to install your project?',
     },
+    // Usage section content
     {
         type: 'input',
         name: 'usage',
         message: 'Provide instructions and examples for use.'
     },
+    // Credits section content
     {
         type: 'input',
         name: 'credits',
         message: 'List your collaborators, if any. Include any other attributions.',
     },
+    // License selection
     {
         type: 'list',
         name: 'license',
@@ -44,21 +50,25 @@ const questions = [
             'None'
         ],
     },
+    // Contributing section content
     {
         type: 'input',
         name: 'contributing',
         message: 'Explain how to contribute to the project.',
     },
+    // Tests section content
     {
         type: 'input',
         name: 'tests',
         message: 'Provide tests for your project.',
     },
+    // Github username
     {
         type: 'input',
         name: 'github',
         message: 'Enter your github username.',
     },
+    // Email
     {
         type: 'input',
         name: 'email',
@@ -66,21 +76,21 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
-    // Generate the markdown for the README
+    // Generate the markdown content for the README
     const markdownContent = generateMarkdown(data);
-    // Write the content to the README file
+    // Write the markdown content to the README file
     fs.writeFile(fileName, markdownContent, err => {
         err ? console.error(err) : console.log('README successfully generated!')
     });
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
-        // Do nothing for now
-    })
+        writeToFile('./README.md', answers);
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 const licenseBadges = {
   'MIT License': 'https://img.shields.io/badge/License-MIT-yellow.svg', 
@@ -7,12 +7,13 @@ const licenseBadges = {
   'BSD 3-Clause License': 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg', 
   'Mozilla Public License 2.0': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg', 
   'The Unlicense': 'https://img.shields.io/badge/license-Unlicense-blue.svg', 
+  'None': '',
 };
 function renderLicenseBadge(license) {
-  return licenseBadges[license] ? `![${license}](${licenseBadges[license]})` : '';
+  return licenseBadges[license] ? `\n![${license}](${licenseBadges[license]})\n` : '';
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 const licenseLinks = {
   'MIT License': 'https://opensource.org/licenses/MIT', 
@@ -21,21 +22,73 @@ const licenseLinks = {
   'BSD 3-Clause License': 'https://opensource.org/licenses/BSD-3-Clause', 
   'Mozilla Public License 2.0': 'https://www.mozilla.org/en-US/MPL/2.0/', 
   'The Unlicense': 'https://unlicense.org/',
+  'None': '',
 };
 function renderLicenseLink(license) {
-  return licenseBadges[license] ? `[${license}](${licenseLinks[license]})` : '';
+  return licenseLinks[license] ? `[${license}](${licenseLinks[license]})` : '';
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+  if (license === 'None' || !license) {
+    return '';
+  } else {
+    return `## License \n
+This project is licensed under the ${license} - see the ${renderLicenseLink(license)} for details.`;
+  }
+}
+
+// Create a function that returns the questions section of README
+function renderQuestionsSection(github, email) {
+  if (github && email) {
+    return `
+If you have any questions about this project, feel free to reach out: \n
+- **GitHub** [${github}](https://github.com/${github})
+- **Email** ${email}
+`;
+  } else if (github) {
+    return `
+If you have any questions about this project, feel free to reach out: \n
+- **GitHub** [${github}](https://github.com/${github})
+`;
+  } else if (email) {
+    return `
+If you have any questions about this project, feel free to reach out: \n
+- **Email** ${email}
+`;
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-    ${renderLicenseBadge(data.license)}
+${renderLicenseBadge(data.license)}
+## Description \n
+${data.description} \n
+## Table of Contents \n
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions) \n
+## Installation \n
+${data.installation} \n
+## Usage \n
+${data.usage} \n
+## Credits \n
+${data.credits} \n
+${renderLicenseSection(data.license)} \n
+## Contributing \n
+${data.contributing} \n
+## Tests \n
+${data.tests} \n
+## Questions \n
+${renderQuestionsSection(data.github,data.email)}
 `;
 }
 
